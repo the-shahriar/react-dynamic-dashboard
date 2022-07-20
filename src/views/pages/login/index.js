cda2bac5304c585c3b8ffa93e499ddeb0883d711
Shahriar Islam <theshahriarislam@gmail.com>
2022-07-20 15:56:59 +0600

    login component added

--- src/views/pages/login/index.js
+++ src/views/pages/login/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import loginImage from "../../../assets/images/login.png";
+import LoginForm from "../../components/login_form";
 
 const Login = () => {
   const onSubmit = (e) => {
@@ -14,40 +15,12 @@ const Login = () => {
       <section className="h-screen bg-gray-50">
         <div className="container px-6 py-12 h-full">
           <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            {/* Image */}
             <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
               <img src={loginImage} className="w-full" alt="Phone" />
             </div>
-            <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
-              <form onSubmit={onSubmit}>
-                {/* Email input */}
-                <div className="mb-6">
-                  <input
-                    name="email"
-                    type="text"
-                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    placeholder="Email address"
-                  />
-                </div>
-                {/* Password input */}
-                <div className="mb-6">
-                  <input
-                    type="password"
-                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    placeholder="Password"
-                    name="password"
-                  />
-                </div>
-                {/* Submit button */}
-                <button
-                  type="submit"
-                  className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
-                >
-                  Login
-                </button>
-              </form>
-            </div>
+            {/* Login form */}
+            <LoginForm onSubmit={onSubmit} />
           </div>
         </div>
       </section>
